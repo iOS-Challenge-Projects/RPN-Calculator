302caebc60411c7858ff0ec725d16136c28d3c78
fritzgt <tuanis@live.com>
2020-04-11 12:48:21 +0300

    Added comments

--- RPN/DigitalAccumulator.swift
+++ RPN/DigitalAccumulator.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+struct DigitAccumulator {
+    enum Digit: Equatable {
+        case decimalPoint
+        case number(Int)
+    }
+    
+    
+    /// Add new digit to the accumulator, throws an error if digit is invalid.
+    /// - Parameter digit: Digit to add
+    mutating func add(digit: Digit) throws {
+        
+    }
+    
+    
+    /// Grab the value out of the digit Accumulator
+    /// - Returns: The value represented by the Digit Accumulator, or nil if it cannot be interpreted as a double
+    func value() -> Double {
+        return 00.00
+    }
+    
+    
+    /// Reset the accumulator
+    mutating func clear()  {
+        
+    }
+}
