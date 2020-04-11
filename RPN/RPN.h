3a0eff980dcfbc5090732dc7d5ffaae03520c5df
fritzgt <tuanis@live.com>
2020-04-11 11:37:39 +0300

    Added rules

---
+++ RPN/RPN.h
@@ -0,0 +1,19 @@
+//
+//  RPN.h
+//  RPN
+//
+//  Created by FGT MAC on 4/11/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RPN.
+FOUNDATION_EXPORT double RPNVersionNumber;
+
+//! Project version string for RPN.
+FOUNDATION_EXPORT const unsigned char RPNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPN/PublicHeader.h>
+
+
