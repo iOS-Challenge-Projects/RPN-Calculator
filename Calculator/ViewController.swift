95549422f68feb13e1f4c35b55c1b80bcaaabd19
fritzgt <tuanis@live.com>
2020-04-11 11:01:49 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by FGT MAC on 4/11/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
