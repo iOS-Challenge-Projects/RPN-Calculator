eb71266bc884ace49f574696216a2295160e5417
fritzgt <tuanis@live.com>
2020-04-11 11:42:58 +0300

    Added stack file to framework

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RPN
 
 class ViewController: UIViewController {
 
---
+++ RPN/Stack.swift
@@ -0,0 +1,9 @@
+//
+//  Stack.swift
+//  RPN
+//
+//  Created by FGT MAC on 4/11/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import Foundation
