576a80ac1316424af9a23da16fd45baf7a957f42
fritzgt <tuanis@live.com>
2020-04-11 14:39:50 +0300

    completed calculator methods

--- RPN/Calculator.swift
+++ RPN/Calculator.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+
+public struct Calculator {
+    
+    public enum Operator {
+        case add
+        case subtract
+        case multiply
+        case devide
+    }
+    
+    private var stack: Stack<Double> = [0.0,0.0]
+    
+    public init () {}
+    
+    
+    private mutating func push(_ number: Double)  {
+        stack.push(number)
+    }
+    
+    
+    public mutating func push(operator: Operator) {
+        let element1 = stack.pop() ?? 0.0
+        let element2 = stack.pop() ?? 0.0
+        
+        let result: Double
+    
+        switch `operator` {
+        case .add:
+            result = element1 + element2
+        case .subtract:
+            result = element1 - element2
+        case .multiply:
+            result = element1 * element2
+        case .devide:
+            result = element1 / element2
+        }
+        stack.push(result)
+    }
+    
+    public var topValue: Double? {
+        return stack.peek()
+    }
+    
+    public mutating func clearStack(){
+        stack = []
+    }
+}
